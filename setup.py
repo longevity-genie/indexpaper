f7266c37924ffd5970a0ebf59aaf2de0c8b37e21
Anton Kulaga <antonkulaga@pop-os.localdomain>
2024-03-20 10:14:31 -0600

    updated deps

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.22'
+VERSION = '0.0.23'
 DESCRIPTION = 'indexpaper - library to index papers with vector databases'
 LONG_DESCRIPTION = 'indexpaper - library to index papers with vector databases'
 
@@ -21,7 +21,7 @@ setup(
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=['pycomfort>=0.0.15', 'hybrid-search>=0.0.10', 'more-itertools', 'click', 'python-dotenv', 'tiktoken', 'FlagEmbedding>=1.2.8',
+    install_requires=['pycomfort>=0.0.15', 'hybrid-search>=0.0.12', 'more-itertools', 'click', 'python-dotenv', 'tiktoken', 'FlagEmbedding>=1.2.8',
                       'langchain>=0.1.12', "langchain-community>=0.0.28", 'openai', 'Deprecated', 'loguru', '',#'fastembed>=0.2.5',
                       'qdrant-client>=1.8.0', 'sentence_transformers', 'datasets', 'polars>=0.20.14', 'beartype'],
     keywords=['python', 'utils', 'files', 'papers', 'download', 'index', 'vector databases'],
